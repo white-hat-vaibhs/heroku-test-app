cf4700283d9f416120329cc01ba06dcc17cc7809
Vaibhav soni <32386700+white-hat-vaibhs@users.noreply.github.com>
2020-04-23 09:36:04 +0530

    init

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <img
+        src= "https://i.ibb.co/tBW7NH1/logo.jpg"
+        alt="Problem?"
+      />
+      <p>BUB Nibbas</p>
+    </header>
+  );
+}
+
+export default Header;
